c2d6fc93b8c33aa7b14444f4c3da0fda04de456c
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-10-05 03:00:13 +0100

    async test with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,23 +1,17 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', function () {
-  it('should return a successful response when success is true', function (done) {
+describe('getPaymentTokenFromAPI', () => {
+  it('should resolve with correct response when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
-        done(); // Call done to signal that the async test is complete
+        expect(response).to.deep.equal({data: 'Successful response from the API'});
+        done();
       })
-      .catch(done); // Call done with the error if the promise is rejected
-  });
-
-  it('should do nothing when success is false', function (done) {
-    getPaymentTokenFromAPI(false)
-      .then((response) => {
-        // Since it does nothing, we want to check if the response is undefined
-        expect(response).to.be.undefined; 
-        done(); // Call done to signal that the async test is complete
-      })
-      .catch(done); // Call done with the error if the promise is rejected
+      .catch((error) => {
+        done(error);
+      });
   });
 });
